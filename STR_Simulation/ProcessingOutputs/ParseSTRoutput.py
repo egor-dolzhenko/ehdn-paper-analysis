b1edf14d89233ff43cbcfac7ffe690c7da94d43c
Phillip Richmond <richmonp@login02.sockeye.arc.ubc.ca>
2019-11-04 11:45:17 -0800

    added simple and complex STR tables which I used for Sim

--- STR_Simulation/ProcessingOutputs/ParseSTRoutput.py
+++ STR_Simulation/ProcessingOutputs/ParseSTRoutput.py
@@ -5,6 +5,9 @@ import argparse
 def GetArgs():
 	parser = argparse.ArgumentParser()
 	parser.add_argument("-C","--Complex",help="These are complex STRs")
+	parser.add_argument("-T","--Table",help="The STR Table for which you are going to collate outputs")
+	parser.add_argument("-D","--Directory",help="The directory which has the output data from the EHdn analysis")
+	parser.add_argument("-O","--Output",help="Output table for results")
 	args = parser.parse_args()
 	return args
 
@@ -13,18 +16,23 @@ def GetArgs():
 # And with a given locus it will search for that locus 
 # And extract the rank and genotyped size
 def ParseSTRetch(stretchInfile,locus):
-
 	return
 
 
 # This function parses an EHDN tsv output from the outlier script
 # And with a given locus it will look for that locus and 
-def ParseEHDN(ehdnInfile,locus):
+def ParseEHDNLocus(ehdnInfile,locus):
+	
 	return
 
 
+def ParseEHDNMotif(ehdnInfile,motif):
+	return
 
 
+def ParseTableCollateOutput(strTableFilename,ehdnOutputDir,outFilename):
+	outfile = open(outFilename,'w')
+	strfile = open(strTableFilename,
 
 
 
@@ -33,7 +41,7 @@ def ParseEHDN(ehdnInfile,locus):
 
 def Main():
 	ARGS = GetArgs()
-
+	
 
 if __name__=="__main__":
     Main()
